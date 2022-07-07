78dec2682d4352e81f3eafd22c79b224f6d50d14
Anton Parfilo <antoxa106@gmail.com>
2022-07-07 09:01:54 +0300

    adaptation

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+const burger = {
+    state : false,
+    setState: (val) => this.state = val,
+    getState: () => this.state,
+    openMenu : (menu, but) => {
+        menu.style.display="flex";
+        but.style.backgroundImage="url('/img/header/close.png')";
+        this.state = true;
+    },
+    closeMenu : (menu, but) => {
+        menu.style.display="none";
+        this.state = false;
+        but.style.backgroundImage="url('/img/header/burger.png')";
+    }
+}
+
+const validateForm = () => {
+    alert("dsa");
+}
+
+
+const burgerMenu = document.getElementById("burger-menu");
+const burgerButton = document.getElementById("burger");
+burgerButton.addEventListener("click", (el) => {
+    burger.getState() ? burger.closeMenu(burgerMenu, burgerButton) : burger.openMenu(burgerMenu, burgerButton)
+});
+const formItems = Array.from(document.getElementsByClassName("form-control"));
+const form = document.getElementById("validateForm");
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+    alert("Спасибо");
+    formItems.forEach(element => {
+        element.value="";
+    });
+});
+
